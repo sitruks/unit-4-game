a293d947b5708f2f91f82c7076e6b4f1259b21fb
SITRUK <kurtis.santillanes@gmail.com>
2019-03-23 09:18:26 -0700

    updated ui scheme for loop population of div assets

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -29,43 +29,95 @@
 
 
 // define button clicks () {
+    
+    // };
+    
+    
+$(document).ready(function() {
+    
+    function ShipStats(name, hp, attackBase, shipNumber, shipActionID, shipID) {
+        this.name = name;
+        this.hp = hp;
+        this.attackBase = attackBase;
+        this.shipNumber = shipNumber;
+        this.shipActionID = shipActionID;
+        this.shipID = shipID;
+    };
+    
+    var halberd = ShipStats('Halberd', 150, 10, 1, "shipAction1", "ship1");
+    // console.log(halberd);
+    var rufus = ShipStats('Rufus', 175, 10, 2, "shipAction2", "ship2");
+    // console.log(rufus);
+    var snake = ShipStats('Snake', 200, 10, 3, "shipAction3", "ship3");
+    // console.log(snake);
+    var larry = ShipStats('Larry', 125, 10, 4, "shipAction4", "ship4");
+    // console.log(larry);
+    
+    var shipsAll = [halberd, rufus, snake, larry];
+    // Test variable pull from array
+    // var rufusHP = shipsAll[1].hp;
+    // console.log(rufusHP);
+    var rufusHP = shipsAll;
+    console.log(rufusHP);
 
-// };
 
+    // for (let i = 0; i < shipsAll.length; i++) {
+    //     return 
+    // }
+  
+    var selectPlayer = false;
+    var selectEnemy = false;
 
-var wins = 0;
-var losses = 0;
-var selectPlayer = false;
-var selectEnemy = false;
-var playerInstructions = [
-    "Choose your ship",
-    "Choose your enemy",
-    "Click the button to retry!",
-];
-var shipStats1 = {
-    name: "Halberd",
-    hp: 150,
-    attack: 10,
-    myAttack: function () {
-
-    },
-};
+    var playerInstructions = [
+        "Choose your ship",
+        "Choose your enemy",
+        "Click the button to retry!",
+    ];
+    
 
 
 
 
-$(document).ready(function clickButton() {
-    $("#buttonShip1").click(function(){
-        if (confirm("Select HALBERD?"));
-    }); 
-    $("#buttonShip2").click(function(){
-        if (confirm("Select SWITCH?"));
-    }); 
-    $("#buttonShip3").click(function(){
-        if (confirm("Select SNAKE?"));
-    }); 
-    $("#buttonShip4").click(function(){
-        if (confirm("Select LARRY?"));
-    }); 
+
+
+
+
+
+
 });
 
+
+
+// wildcard calling script
+// $('[id^=success][id$=' + number + ']').show();
+
+// // buttons that alert with TF logic
+// $(document).ready(function clickButton() {
+//     $("#buttonShip1").click(function(){
+//         if (confirm("Select HALBERD?"));
+//     }); 
+//     $("#buttonShip2").click(function(){
+//         if (confirm("Select RUFUS?"));
+//     }); 
+//     $("#buttonShip3").click(function(){
+//         if (confirm("Select SNAKE?"));
+//     }); 
+//     $("#buttonShip4").click(function(){
+//         if (confirm("Select LARRY?"));
+//     }); 
+// });
+
+
+// appply function from mdn
+// function ShipAttacks(obj, objName) {
+//     var result = '';
+//     for (var i in obj) {
+//       // obj.hasOwnProperty() is used to filter out properties from the object's prototype chain
+//       if (obj.hasOwnProperty(i)) {
+//         result += objName + '.' + i + ' = ' + obj[i] + '\n';
+//       }
+//     }
+//     return result;
+// };
+
+// console.log(ShipAttacks());
